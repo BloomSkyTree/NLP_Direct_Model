5be151803d2898a4d7808af6858ae8c14dbcea34
z1z234z <634778353@qq.com>
2017-12-25 01:10:16 +0800

    add directory choice

--- read_file.py
+++ read_file.py
@@ -6,20 +6,30 @@ class read_file_part:
     positive = ['PA', 'PE', 'PD', 'PH', 'PG', 'PB', 'PK']
     negative = ['NA', 'NB', 'NJ', 'NH', 'PF', 'NI', 'NC', 'NG', 'NE', 'ND', 'NN', 'NK', 'NL', 'PC']
 
-    extreme = ""        ##极其|extreme / 最|most           2
-    very    = ""        ##很|very                          1.25
-    more    = ""        ##较|more                          1.2
-    _ish    = ""        ##稍|-ish                          0.8
-    insufficiently = "" ##欠|insufficiently                0.5
-    over    = ""        ##超|over                          1.5
-    noword  = ""        ##否定词
-    emotion = []        ##情感词
-    emotionnum   = 0    ##情感词总数
+    extreme = ""            ##极其|extreme / 最|most           2
+    very    = ""            ##很|very                          1.25
+    more    = ""            ##较|more                          1.2
+    _ish    = ""            ##稍|-ish                          0.8
+    insufficiently = ""     ##欠|insufficiently                0.5
+    over    = ""            ##超|over                          1.5
+    noword  = ""            ##否定词
+    dligemotion = []        ##大连理工情感词
+    emotionnum   = 0        ##情感词总数
+    hownetposemotion = []   ##知网hownet正向情感词
+    hownetnegemotion = []   ##知网hownet负向情感词
+    tsingposemotion = []    ##TSing正向情感词
+    tsingnegemotion = []    ##TSing负向情感词
 
     ##路径
     HownetPath = 'SentimentAnalysisDic'+u'/知网Hownet情感词典'
+    HownetPosEmoFile = u'/正面情感词语（中文）' + '.txt'
+    HownetPosEvaFile = u'/正面评价词语（中文）' + '.txt'
+    HownetNegEmoFile = u'/负面情感词语（中文）' + '.txt'
+    HownetNegEvaFile = u'/负面评价词语（中文）' + '.txt'
     NegatePath = 'SentimentAnalysisDic' + u'/否定词典' + u'/否定' + '.txt'
     DLLGPath = 'SentimentAnalysisDic' + u'/大连理工情感词汇本体' + u'/情感词汇本体' + '.xlsx'
+    TsinghuaPosFile = 'SentimentAnalysisDic' + u'/清华大学李军中文褒贬义词典' + '/tsinghua.positive.gb.txt'
+    TsinghuaNegFile = 'SentimentAnalysisDic' + u'/清华大学李军中文褒贬义词典' + '/tsinghua.negative.gb.txt'
 
     ##读取文件函数
     def _read_file_(self, filename):
@@ -43,6 +53,30 @@ class read_file_part:
             word[i-1] = sh.row_values(i)
         return word
 
+    ##读取知网Hownet情感词典
+    def _read_hownet_emotion_file(self, filename1, filename2):
+        word = []
+        file_object1 = open(filename1)
+        file_object2 = open(filename2)
+        for line in file_object1:
+            line = line.strip()
+            word.append(line)
+        for line in file_object2:
+            line = line.strip()
+            word.append(line)
+        file_object1.close()
+        file_object2.close()
+        return word
+
+    ##读取清华大学李中情感词典
+    def _read_ts_emotion_file(self, filename1):
+        word = []
+        file_object1 = open(filename1)
+        for line in file_object1:
+            line = line.strip()
+            word.append(line)
+        file_object1.close()
+        return word
 
     def __init__(self):
         self.extreme = self._read_file_(self.HownetPath +'/extreme.txt')
@@ -52,4 +86,8 @@ class read_file_part:
         self.insufficiently = self._read_file_(self.HownetPath +'/insufficiently.txt')
         self.over = self._read_file_(self.HownetPath +'/over.txt')
         self.noword = self._read_file_(self.NegatePath)
-        self.emotion = self._read_dllg_emotion_file()
+        self.dligemotion = self._read_dllg_emotion_file()
+        self.hownetposemotion = self._read_hownet_emotion_file(self.HownetPath + self.HownetPosEmoFile, self.HownetPath + self.HownetPosEvaFile)
+        self.hownetnegemotion = self._read_hownet_emotion_file(self.HownetPath + self.HownetNegEmoFile, self.HownetPath + self.HownetNegEvaFile)
+        self.tsingposemotion = self._read_ts_emotion_file(self.TsinghuaPosFile)
+        self.tsingnegemotion = self._read_ts_emotion_file(self.TsinghuaNegFile)
